412541d5e353bbb850ee71a7ad31812f400fea86
kudlada <ladakudlackova@email.cz>
2021-08-10 21:18:36 +0200

    Added code to detect button short press.

--- Button.h
+++ Button.h
@@ -1,24 +1,45 @@
 #ifndef Button_h
 #define Button_h
 
+/*
+  Modified Arduino code for detecting the short press.
+  https://arduinogetstarted.com/tutorials/arduino-button-long-press-short-press
+*/
+
 class Button {
   
 public:
   Button(){
-      
   }
 
-  void setPin(int button_pin)
-  {
+  void setPin(int button_pin){
     pin = button_pin;
-    pinMode(pin, INPUT);
+    pinMode(pin, INPUT_PULLUP);
   }
 
   bool isPressed(){
-    return (digitalRead(pin) == HIGH);
+    currentState = digitalRead(pin);
+
+    if(lastState == HIGH && currentState == LOW)        // button is pressed
+      pressedTime = millis();
+    else if(lastState == LOW && currentState == HIGH) { // button is released
+      releasedTime = millis();
+
+    long pressDuration = releasedTime - pressedTime;
+    if( pressDuration < SHORT_PRESS_TIME )
+      Serial.println("A short press is detected");
+  }
+
+  lastState = currentState;
   }
   
 private:
+  const int SHORT_PRESS_TIME = 500; // 500 milliseconds
   int pin;
+  
+  int lastState = HIGH;  
+  int currentState;     
+  unsigned long pressedTime  = 0;
+  unsigned long releasedTime = 0;
 };
 #endif
