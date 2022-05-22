438608baca5641fa16ac7a4778cab20af95dd2f4
LadaKa <LadaKudlackova@seznam.cz>
2022-05-22 16:10:01 +0200

    Added sketch to test button press.
    Working fine (min time between presses: 500 millisec).

---
+++ test_sketches/sketch_may21a/sketch_may21a.ino
@@ -0,0 +1,50 @@
+
+#define MIN_PULSE  500
+#define MAX_PULSE 2500
+#define LEFT_PIN  12
+#define RIGHT_PIN 13
+#define BUTTON_PIN 2
+
+#define LED 11
+
+#define SPEED 200
+#define TURN_SPEED 30
+
+/*
+ *  Test of button:
+ *  works fine.
+ */
+int lastButtonPressTime = 0;
+volatile bool buttonPressed = false;
+
+void setup() {
+  //  button setup
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), onButtonPressed, FALLING);
+
+  //  led setup
+  pinMode(LED, OUTPUT);
+
+}
+
+// opakujici se smyčka
+void loop() {
+  if (buttonPressed)
+  {
+    buttonPressed = false;
+    digitalWrite(LED, HIGH); //zapnutí led
+    delay(1000); // po dobu jedné sekundy
+    digitalWrite(LED, LOW); // vypnuti LED
+  }
+}
+
+
+// button press handler (using interrupt)
+void onButtonPressed() {
+  int currentTime = millis();
+  if ((currentTime - lastButtonPressTime) < 500)
+    return;
+
+ // lastButtonPressTime = currentTime;
+  buttonPressed = true;
+}
